357c824717609b54de56b453b662acd12cc0fed0
EricWendel <eawendel19@gmail.com>
2020-12-31 16:12:56 -0600

    Distance Function
    Completed the distance formula function for two 2D points

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function distance(){
 	let theForm = document.forms["distance-form"];
 	let x1 = theForm.elements[0].value;
-	document.getElementById('ans').innerHTML = "x1: "+x1; //for testing
+	let y1 = theForm.elements[1].value;
+	let x2 = theForm.elements[2].value;
+	let y2 = theForm.elements[3].value;
+	let dist = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+	document.getElementById('ans').innerHTML = "Answer: " + dist;
 }
