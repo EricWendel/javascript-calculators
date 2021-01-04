d7c15c3476d5aaa1fa46e977b59b4eeea9f8399b
EricWendel <eawendel19@gmail.com>
2021-01-03 20:19:59 -0600

    Set Default Input Values
    Blank entries in the calculator set themselves to 0 in the input box when calculated

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ function distance(){
 	let y1 = theForm.elements[1].value;
 	let x2 = theForm.elements[2].value;
 	let y2 = theForm.elements[3].value;
+	if(x1 == 0){
+		document.getElementById('x1').value = 0;
+	}
+	if(x2 == 0){
+		document.getElementById('x2').value = 0;
+	}
+	if(y1 == 0){
+		document.getElementById('y1').value = 0;
+	}
+	if(y2 == 0){
+		document.getElementById('y2').value = 0;
+	}
 	let dist = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
 	document.getElementById('ans').innerHTML = "Answer: " + dist;
 }
